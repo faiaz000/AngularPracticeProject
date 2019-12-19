c22de55667d0b63742593b79f70cf70f2e35941d
faiaz000 <faiaz000@hotmail.com>
2019-12-19 17:04:08 +0900

    routing seperated for auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostCreateComponent } from './components/post-create/post-create.component'
-import { LoginComponent } from './components/auth/login/login.component';
-import { SignupComponent } from './components/auth/signup/signup.component';
 import { AuthGuard } from './guard/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: PostListComponent},
-  { path:'login', component: LoginComponent},
-  { path:'signup', component: SignupComponent},
   { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard]},
-  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]}
+  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]},
+  { path:'auth', loadChildren: () => import('./components/auth/auth.module').then(mod => mod.AuthModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './components/post-create/post-create.component';
 import { HeaderComponent } from './components/header/header.component';
-import { PostListComponent } from './components/post-list/post-list.component';
 import { PostsService } from './services/posts.service';
-import { LoginComponent } from './components/auth/login/login.component';
-import { SignupComponent } from './components/auth/signup/signup.component';
+
+
 import { AuthInterceptor } from './interceptor/auth-interceptor';
 import { ErrorInterceptor } from './interceptor/error-interceptor';
 import { ErrorComponent } from './components/error/error.component';
-import { AngularMaterialModule } from './angular-material.module';
+import { AngularMaterialModule } from './modules/angular-material.module';
+import { PostsModule } from './modules/posts.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
     HeaderComponent,
-    PostListComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    PostsModule
   ],
   providers: [PostsService , 
     {provide:HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true},
